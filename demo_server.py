0ccc9e7c31c3daba5db608fdeaf0d77740befeae
AyaRabea <51236433+AyaRabea@users.noreply.github.com>
2021-04-05 22:29:38 +0200

    Update demo_server.py

--- demo_server.py
+++ demo_server.py
@@ -90,6 +90,7 @@ if __name__ == '__main__':
   print(hparams_debug_string())
   synthesizer.load(args.checkpoint)
   print('Serving on port %d' % args.port)
-  simple_server.make_server('0.0.0.0', args.port, api).serve_forever()
+  #simple_server.make_server('0.0.0.0', args.port, api).serve_forever()
+  api.run(host='0.0.0.0',port=5000)
 else:
   synthesizer.load(os.environ['CHECKPOINT'])
